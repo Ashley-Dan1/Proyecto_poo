9c06d1a986cc78216ce579d4a83205c122f3c90e
Ashley-a-a <ashley.15.04.2004@gmail.com>
2026-05-27 22:04:05 -0500

    Update controller.py

--- Bloques en flask/app/blueprints/bloque05/controller.py
+++ Bloques en flask/app/blueprints/bloque05/controller.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request
 from app.blueprints.bloque05.models import (
     ejecutar_ejercicio1, ejecutar_ejercicio2, ejecutar_ejercicio3
 )
+from app.contenido import COMPENDIO   # ← import del compendios
 
 b05_bp = Blueprint('bloque05', __name__, template_folder='../../templates')
 
@@ -22,6 +23,19 @@ DATOS_RETOS = {
         "es_interactivo": True
     }
 }
+
+@b05_bp.route('/concepto')
+def ver_concepto():
+    info = COMPENDIO.get("bloque05", {})
+    return render_template(
+        'ejercicio_concepto.html',          # nombre corregido (sin typo)
+        bloque_id="bloque05",
+        bloque_titulo=info.get("titulo", "Bloque 05"),
+        concepto_texto=info.get("concepto", ""),
+        ejemplo_codigo=info.get("ejemplo", ""),
+        datos_retos_nav=list(DATOS_RETOS.keys())
+    )
+ 
 
 @b05_bp.route('/ejercicio/<int:num_ej>', methods=['GET', 'POST'])
 def gestionar_ejercicio(num_ej):
@@ -60,4 +74,5 @@ def gestionar_ejercicio(num_ej):
         es_interactivo=reto["es_interactivo"],
         consola=salida_consola,
         datos_retos_nav=list(DATOS_RETOS.keys())
+    )
     )
