70badf3c0848bc14e701f8ee3e331f3fa26fe5b7
Ashley-a-a <ashley.15.04.2004@gmail.com>
2026-05-27 22:08:53 -0500

    Update controller.py

--- Bloques en flask/app/blueprints/bloque09/controller.py
+++ Bloques en flask/app/blueprints/bloque09/controller.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request
 from app.blueprints.bloque09.models import (
     ejecutar_ejercicio1, ejecutar_ejercicio2, ejecutar_ejercicio3
 )
+from app.contenido import COMPENDIO   # ← import del compendio
 
 b09_bp = Blueprint('bloque09', __name__, template_folder='../../templates')
 
@@ -22,6 +23,19 @@ DATOS_RETOS = {
         "es_interactivo": True
     }
 }
+
+@b09_bp.route('/concepto')
+def ver_concepto():
+    info = COMPENDIO.get("bloque09", {})
+    return render_template(
+        'ejercicio_concepto.html',          # nombre corregido (sin typo)
+        bloque_id="bloque09",
+        bloque_titulo=info.get("titulo", "Bloque 09"),
+        concepto_texto=info.get("concepto", ""),
+        ejemplo_codigo=info.get("ejemplo", ""),
+        datos_retos_nav=list(DATOS_RETOS.keys())
+    )
+ 
 
 @b09_bp.route('/ejercicio/<int:num_ej>', methods=['GET', 'POST'])
 def gestionar_ejercicio(num_ej):
